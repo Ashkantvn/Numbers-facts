26597e93a0b44503c3ba823a525406f81239ccdf
AshkanTavangar <ashkantavangar7@gmail.com>
2022-09-10 13:16:27 +0430

    Adding React-Query and initialize it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query/build/types/packages/react-query/src/QueryClientProvider";
 function App() {
+  const client = new QueryClient();
   return (
-    <div className="container">
-      App
-    </div>
+    <QueryClientProvider client={client}>
+      <div className="container">App</div>
+    </QueryClientProvider>
   );
 }
 
