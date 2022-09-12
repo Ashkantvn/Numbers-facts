a4351bab19cb411293cfdbaa939f8187da676000
AshkanTavangar <ashkantavangar7@gmail.com>
2022-09-12 20:43:32 +0430

    Add class name to numbers and decrease numbers array

--- src/parts/styled-numbers/styledNumbers.tsx
+++ src/parts/styled-numbers/styledNumbers.tsx
@@ -1,5 +1,7 @@
+import "./styledNumbers.css";
+
 const StyledNumbers = () => {
-  const randomNumbers = [1, 5, 8, 9, 6, 4, 7, 2, 3];
+  const randomNumbers = [1, 5, 8, 9 ,3];
   const mappedRandomNumbers = randomNumbers.map((number) => {
     return <span className={"number-"+number} key={number}>{number}</span>;
   });
